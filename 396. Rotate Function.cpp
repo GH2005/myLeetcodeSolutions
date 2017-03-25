0c93ffcd66bb10ecbefc35a9ed02d05f5c864445
Yang Wang <pitsou7@gmail.com>
2017-03-25 11:03:29 -0300

    Update 396. Rotate Function.cpp

--- 396. Rotate Function.cpp
+++ 396. Rotate Function.cpp
@@ -1,21 +1,15 @@
 class Solution {
-    int exclusiveSum[100000];
 public:
     int maxRotateFunction(vector<int>& A) {
         int len = A.size();
         if (len <= 1) return 0;
         int sum = 0;
-        for (int pos = 0; pos < len; pos++) sum += A[pos];
-        for (int pos = 0; pos < len; pos++) this->exclusiveSum[pos] = sum - A[pos];
         int maxF = 0;
-        for (int pos = 0; pos < len; pos++) maxF += pos * A[pos];
-        int prev = maxF;
-        int start = 0;
-        for (int k = 0; k < len; k++) {
-            start = (start + len - 1) % len;
-            int currF = prev + this->exclusiveSum[start] - (len-1)*A[start];
+        for (int pos = 0; pos < len; pos++) { sum += A[pos]; maxF += pos * A[pos]; }
+        int currF = maxF;
+        for (int k = len - 1; 0 < k; --k) {
+            currF = currF + sum - len * A[k];
             maxF = max(maxF, currF);
-            prev = currF;
         }
         return maxF;
     }
