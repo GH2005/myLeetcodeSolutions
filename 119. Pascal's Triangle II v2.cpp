b290ad78c539120601b88793b1db0b0085e269a5
Yang Wang <pitsou7@gmail.com>
2017-05-30 23:25:40 -0300

    Update 119. Pascal's Triangle II v2.cpp

--- 119. Pascal's Triangle II v2.cpp
+++ 119. Pascal's Triangle II v2.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> res(rowIndex+1, 1);
-        for (int row = 0; row <= rowIndex; row++)
+        for (int row = 2; row <= rowIndex; row++)
             for (int pos = row-1; 1 <= pos; pos--)
                 res[pos] = res[pos] + res[pos-1];
         return res;
