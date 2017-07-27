48635591c689630768e2f819f034c8a954bee109
Yang Wang <pitsou7@gmail.com>
2017-07-27 18:27:21 -0300

    Update 461. Hamming Distance.cpp

--- 461. Hamming Distance.cpp
+++ 461. Hamming Distance.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         int res = 0;
-        for (int xx = x ^ y; xx != 0; xx >>= 1)
-            if ((xx & 1) == 1) res++;
+        for (int xx = x ^ y; xx != 0; xx &= xx-1)
+            res++;
         return res;
     }
 };
