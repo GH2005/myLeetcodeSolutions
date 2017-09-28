3566c01642ea8bdd8a0f0f240244e140bb2a07f4
Yang Wang <pitsou7@gmail.com>
2017-09-28 11:35:26 -0300

    Create 492. Construct the Rectangle.cpp

---
+++ 492. Construct the Rectangle.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int width = sqrt(area);
+        while (area % width != 0) width--;
+        return {area/width, width};
+    }
+};
